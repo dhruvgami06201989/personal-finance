46146d2f34eadd3b4e72423f5da735067855e145
dhruvgami06201989 <dhruv.gami.1989@gmail.com>
2020-08-25 19:34:02 -0500

    Updated for 2 banks
    # Multiprocessing dataframes added

--- calculate_monthly_expense.py
+++ calculate_monthly_expense.py
@@ -5,6 +5,7 @@ import json
 import glob
 import pandas as pd
 from file_path_generator import FilePathGenerator
+import multiprocessing as mp
 
 
 class ProcessStatement:
@@ -13,7 +14,7 @@ class ProcessStatement:
         self.general_categ_dict = None
         self.specific_categ_dict = None
 
-    def update_mapping(self):
+    def update_mapping(self, log_txt):
         mapping_filename = self.fpg.mapping_file('Category Mapping')
         general_mapping_dict_file = self.fpg.mapping_file('Stored General Maps')
         specific_mapping_dict_file = self.fpg.mapping_file('Stored Specific Maps')
@@ -21,6 +22,7 @@ class ProcessStatement:
         if os.path.isfile(mapping_filename):
             map_file_update_datetime = datetime.datetime.fromtimestamp(os.path.getmtime(mapping_filename))
         else:
+            log_txt.write(">> No Mapping file found!!\n")
             sys.exit(">> No Mapping file found!!")
 
         update_flag = 0
@@ -31,16 +33,10 @@ class ProcessStatement:
         else:
             update_flag = 1
 
-        # update_flag = 0
-        # file_update_date = datetime.datetime.fromtimestamp(os.path.getmtime(mapping_filename)).date()
-        # if file_update_date >= (datetime.datetime.today().date() - datetime.timedelta(days=30)):
-        #     update_flag = 1
-
         if update_flag == 1:
+            log_txt.write(">> Mapping Dict files need to be updated\n")
             general_categ_map_df = pd.read_excel(mapping_filename, sheet_name="General")
             specific_categ_map_df = pd.read_excel(mapping_filename, sheet_name="Specific")
-            # uniq_general_categ = general_categ_map_df['Category'].unique().tolist()
-            # uniq_specf_categ = general_categ_map_df['Category'].unique().tolist()
 
             general_categ_dict = dict(zip(general_categ_map_df['Description'], general_categ_map_df['Category']))
 
@@ -53,6 +49,7 @@ class ProcessStatement:
                 json.dump(specific_categ_dict, dict_file)
 
         else:
+            log_txt.write(">> No need to update mapping dict files\n")
             with open(general_mapping_dict_file, 'r') as dict_file:
                 general_categ_dict = json.load(dict_file)
 
@@ -62,78 +59,173 @@ class ProcessStatement:
         self.general_categ_dict = general_categ_dict
         self.specific_categ_dict = specific_categ_dict
 
-        # return general_categ_dict, specific_categ_dict
+        log_txt.write(">> Mapping dict files updated successfully!\n")
 
-    def allocate_category(self, statement_df, accnt_str):
-        if accnt_str == '3133' or '2703' or '3103':
-            descr_col_name = 'Description'
-            categ_col_name = 'Category'
+    def get_raw_bank_statements(self):
+        bank_ls = ['Chase', 'DCU', 'Fifth Third']
+        raw_statement_df_dict = {}
+        for bank in bank_ls:
+            file_ls = glob.glob(self.fpg.bank_statements(bank) + '/*.csv')
+            if bank == 'Chase':
+                account_ls = ['3133', '2703', '3103', '1862']
+            elif bank == 'DCU':
+                account_ls = ['Checking', 'Savings']
+            elif bank == 'Fifth Third':
+                continue
+            for each_accnt in account_ls:
+                key = bank + '-' + each_accnt
+                account_file_ls = [x for x in file_ls if each_accnt in x]
+                acct_file_latest = max(account_file_ls, key=os.path.getctime)
+                if bank == 'DCU':
+                    each_acct_df = pd.read_csv(acct_file_latest, header=3)
+                    each_acct_df['Memo'] = each_acct_df['Memo'].fillna(each_acct_df['Description'])
+                    each_acct_df['Amount Debit'] = each_acct_df['Amount Debit'].fillna(0)
+                    each_acct_df['Amount Credit'] = each_acct_df['Amount Credit'].fillna(0)
+                    each_acct_df.loc[:, 'Amount'] = each_acct_df['Amount Debit'] + each_acct_df['Amount Credit']
+                else:
+                    each_acct_df = pd.read_csv(acct_file_latest)
+                each_acct_df.loc[:, 'Credit/Debit'] = each_acct_df['Amount'].apply(lambda x:
+                                                                                   'Credit' if x > 0 else 'Debit')
+                raw_statement_df_dict.update({key: each_acct_df})
+        return raw_statement_df_dict
 
-        chase_3133_updated_df = pd.DataFrame()
+    def allocate_category(self, statement_df, bank, accnt_str):
+        if bank == 'Chase':
+            description_col = 'Description'
+            if accnt_str == '3133' or accnt_str == '2703':
+                post_date_col = 'Post Date'
+                type_col = 'Type'
+            elif accnt_str == '3103' or accnt_str == '1862':
+                post_date_col = 'Posting Date'
+                type_col = 'Details'
+        elif bank == 'DCU':
+            description_col = 'Memo'
+            post_date_col = 'Date'
+            type_col = 'Description'
+
+        key = bank + '-' + accnt_str
+
+        exception_dict = {'ATM CHECK DEPOSIT 04/01 2943 RICHLAND AVE LOUISVILLE KY': 'Tax/Interest'}
+        updated_statement_dict = {}
+        statement_updated_df = pd.DataFrame()
         for idx, row in statement_df.iterrows():
             substring_match = [substring for substring in self.general_categ_dict.keys() if substring.upper() in
-                               row['Description'].upper()]
+                               row[description_col].upper()]
             if substring_match:
                 if len(substring_match) == 1:
                     category = self.general_categ_dict[substring_match[0]]
+                    mapping_found = 'General-Single'
                 else:
-                    print("Multiple matching substrings found for: " + row['Description'] +
+                    print("Multiple matching substrings found for: " + row[description_col] +
                           " in General Mapping. Category assigned: " + self.general_categ_dict[substring_match[0]])
-                    category = self.general_categ_dict[substring_match[0]]
+                    categories = [self.general_categ_dict[x] for x in substring_match]
+                    category = categories[0]
+                    mapping_found = 'General-Multiple: ' + ", ".join(categories) + ". Selected: " + category
             else:
                 substring_match = [substring for substring in self.specific_categ_dict.keys() if
-                                   substring.upper() in row['Description'].upper()]
+                                   substring.upper() in row[description_col].upper()]
                 if substring_match:
                     if len(substring_match) == 1:
                         category = self.specific_categ_dict[substring_match[0]]
+                        mapping_found = 'Specific-Single'
                     else:
-                        print("Multiple matching substrings found for: " + row['Description'] +
+                        print("Multiple matching substrings found for: " + row[description_col] +
                               " in Specific Mapping. Category assigned: " + self.specific_categ_dict[substring_match[0]])
-                        category = self.specific_categ_dict[substring_match[0]]
+                        categories = [self.specific_categ_dict[x] for x in substring_match]
+                        category = categories[0]
+                        mapping_found = 'Specific-Multiple: ' + ", ".join(categories) + ". Selected: " + category
                 else:
-                    print("No matching Category for: " + str(row['Description']))
+                    print("No matching Category for: " + str(row[description_col]))
                     category = 'Other'
+                    mapping_found = "None. Selected " + category
 
-            if chase_3133_updated_df.empty:
-                chase_3133_updated_df = pd.DataFrame(
-                    data=[[row['Post Date'], row['Description'], row['Type'], row['Amount'], category]],
-                    columns=['Post Date', 'Description', 'Type', 'Amount', 'Category'])
+            if row[description_col] in list(exception_dict.keys()):
+                category = exception_dict[row[description_col]]
+
+            if statement_updated_df.empty:
+                statement_updated_df = pd.DataFrame(
+                    data=[[row[post_date_col], row[description_col], row[type_col], row['Amount'], category,
+                           mapping_found, row['Credit/Debit'], bank, accnt_str]],
+                    columns=['Date', 'Description', 'Type', 'Amount', 'Category', 'Mapping Reason', 'Credit/Debit',
+                             'Bank', 'Acct Num'])
+                updated_statement_dict.update({key: statement_updated_df})
             else:
-                chase_3133_updated_df = chase_3133_updated_df.append(pd.DataFrame(
-                    data=[[row['Post Date'], row['Description'], row['Type'], row['Amount'], category]],
-                    columns=['Post Date', 'Description', 'Type', 'Amount', 'Category']))
+                statement_updated_df = statement_updated_df.append(pd.DataFrame(
+                    data=[[row[post_date_col], row[description_col], row[type_col], row['Amount'], category,
+                           mapping_found, row['Credit/Debit'], bank, accnt_str]],
+                    columns=['Date', 'Description', 'Type', 'Amount', 'Category', 'Mapping Reason', 'Credit/Debit',
+                             'Bank', 'Acct Num']))
+                updated_statement_dict.update({key: statement_updated_df})
         print("debug")
+        return updated_statement_dict
 
 
-def main():
-    fpg = FilePathGenerator()
+class MultiprocessStatements:
+    def __init__(self):
+        self.result = {}
+
+    def collect_results(self, result):
+        self.result.update(result)
+
+
+def main(run_date):
+    run_date_str = run_date.strftime("%Y-%m-%d")
+    fpg = FilePathGenerator(run_date)
+    log_file = fpg.output_file('Log') + '\\' + 'Log_' + run_date_str + '.log'
+    log_txt = open(log_file, 'w')
+
     statement_processor = ProcessStatement(fpg)
-    statement_processor.update_mapping()
+    log_txt.write(">> Updating mapping file.....\n")
+    statement_processor.update_mapping(log_txt)
+    cores = mp.cpu_count()
+    pool = mp.Pool(processes=cores)
+    statement_multiprocessor = MultiprocessStatements()
 
-    chase_file_ls = glob.glob(fpg.bank_statements('Chase') + '/*.csv')
-    chase_3133_file_ls = [x for x in chase_file_ls if '3133' in x]
-    chase_2703_file_ls = [x for x in chase_file_ls if '2703' in x]
-    chase_3103_file_ls = [x for x in chase_file_ls if '3103' in x]
-    chase_3133_file_latest = max(chase_3133_file_ls, key=os.path.getctime)
-    chase_2703_file_latest = max(chase_2703_file_ls, key=os.path.getctime)
-    chase_3103_file_latest = max(chase_3103_file_ls, key=os.path.getctime)
+    log_txt.write(">> Get Raw bank statements....\n")
+    raw_statement_df_dict = statement_processor.get_raw_bank_statements()
+    log_txt.write(">> Raw Bank Statements fetched successfully\n")
+    log_txt.write(">> Updating categories for all bank statements using parallel processing\n")
+    for key, each_acct_raw_df in raw_statement_df_dict.items():
+        bank = key.split('-')[0]
+        account = key.split('-')[1]
+        # ------------------- TEMPORARY DEBUGGING -----------------
+        # if key == 'DCU-Checking' or key == 'DCU-Savings':
+        #     bank = key.split('-')[0]
+        #     account = key.split('-')[1]
+        #     raw_acct_df = raw_statement_df_dict[key]
+        #     each_updated_bank_df_dict = statement_processor.allocate_category(raw_acct_df, bank, account)
+        #     statement_multiprocessor.collect_results(each_updated_bank_df_dict)
+        # ---------------------------------------------------------
+        pool.apply_async(statement_processor.allocate_category, args=(each_acct_raw_df, bank, account),
+                         callback=statement_multiprocessor.collect_results)
+    pool.close()
+    pool.join()
+    log_txt.write(">> Updated all statements successfully\n")
 
-    chase_3133_df = pd.read_csv(chase_3133_file_latest)
-    chase_2703_df = pd.read_csv(chase_2703_file_latest)
-    chase_3103_df = pd.read_csv(chase_3103_file_latest)
+    updated_bank_stat_dict = statement_multiprocessor.result
+    combined_new_bank_statement = pd.DataFrame()
+    for each_bank_acct_df in updated_bank_stat_dict.values():
+        combined_new_bank_statement = combined_new_bank_statement.append(each_bank_acct_df, sort=False)
 
-    statement_processor.allocate_category(chase_3133_df, '3133')
-    print("debug")
+    combined_new_bank_statement.sort_values(by='Date', ascending=False)
+    combined_new_bank_statement = combined_new_bank_statement.drop_duplicates()
 
-    chase_df_ls = [chase_3133_df, chase_2703_df, chase_3103_df]
-    # for each_chase_df in chase_df_ls:
+    all_statement_combined_filename = fpg.output_file('Combined Statement All')
+    if os.path.isfile(all_statement_combined_filename):
+        all_statement_combined_df = pd.read_csv(all_statement_combined_filename)
+        all_statement_combined_df = all_statement_combined_df.append(combined_new_bank_statement, sort=False)
+        all_statement_combined_df = all_statement_combined_df.drop_duplicates()
+        all_statement_combined_df.to_csv(all_statement_combined_filename, index=False)
+        combined_new_bank_statement.to_csv(fpg.output_file('Combined Statement New'), index=False)
+    else:
+        combined_new_bank_statement.to_csv(fpg.output_file('Combined Statement All'), index=False)
 
-    return
+    log_txt.write(">> Run Completed on: " + run_date_str)
+    print(">> Run Completed on: " + run_date_str)
+    log_txt.close()
 
 
 if __name__ == "__main__":
-    # file_path_gen = FilePathGenerator()
-    # statement_processor = ProcessStatement(file_path_gen)
-    # statement_processor.update_mapping()
-    main()
+    run_date = datetime.datetime.today()
+    main(run_date)
     print("End of Run")
--- file_path_generator.py
+++ file_path_generator.py
@@ -4,8 +4,10 @@ import datetime
 
 
 class FilePathGenerator:
-    def __init__(self):
+    def __init__(self, run_date):
         self.working_dir = r'C:\Dhruv\Project Data\Personal Finance'
+        self.log_dir = self.working_dir + '\\' + 'log'
+        self.run_date = run_date
 
     def bank_statements(self, bank):
         if bank == 'Chase':
@@ -17,14 +19,27 @@ class FilePathGenerator:
         os.makedirs(file_path, exist_ok=True)
         return file_path
 
-    def mapping_file(self, type):
+    def mapping_file(self, file_type):
         file_path = self.working_dir + '\\' + 'Internal'
-        if type == 'Category Mapping':
+        if file_type == 'Category Mapping':
             file_name = file_path + '\\' + 'Category Mapping.xlsx'
             return file_name
-        if type == 'Stored General Maps':
+        if file_type == 'Stored General Maps':
             file_name = file_path + '\\' + 'General Mapping.json'
             return file_name
-        if type == 'Stored Specific Maps':
+        if file_type == 'Stored Specific Maps':
             file_name = file_path + '\\' + 'Specific Mapping.json'
             return file_name
+
+    def output_file(self, file_type):
+        if file_type == 'Combined Statement New':
+            file_name = self.working_dir + '\\' + 'Combined_Statement_' + self.run_date.strftime("%Y-%m-%d") + '.csv'
+            return file_name
+        if file_type == 'Combined Statement All':
+            file_name = self.working_dir + '\\' + 'Combined_Statement_All.csv'
+            return file_name
+        if file_type == 'Log':
+            log_dir = self.working_dir + '\\' + 'Logs'
+            os.makedirs(log_dir, exist_ok=True)
+            return log_dir
+
